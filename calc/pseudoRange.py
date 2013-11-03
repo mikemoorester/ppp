1eba9a38060be2bfe870f586ac34c3d83ca4fb19
mikemoorester <mikemoorester@gmail.com>
2013-11-03 23:41:55 +1100

    Added a placeholder to calculate position from pseudorange code

---
+++ calc/pseudoRange.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import numpy as np
+
+def noApriori_position():
+    """
+    Calculate a position, with no apriori coordinates from pseudorange observation
+ 
+    position()
+
+
+    """
+
+    Aguess = np.array([ 0, 0, 0])
+
+    SatPos = np.array(
+                      [15524471.175, -16649826.222, 13512272.387],
+                      [ ]
+                      [ ]
+                      [ ]
+                     )
+    #Range = Sv - Aguess
+
+if __name__ == "__main__":
+    
+    noApriori_position()
+
